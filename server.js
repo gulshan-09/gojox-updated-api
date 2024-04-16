9e3d44828aa5730bfa97d2504f920efa82664a20
gulshan-09 <152474356+gulshan-09@users.noreply.github.com>
2024-04-16 11:49:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,25 +5,33 @@ const router = express.Router();
 require("./db/conn");
 const cors = require('cors');
 const controllers = require("./controllers/dataController");
+
 const PORT = process.env.PORT || "5001";
+const API_KEY = process.env.API_KEY;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json("Server Start");
+  res.status(200).json("You are quit cleaver but not more than me. You can't access my database.😉😎");
 });
 
-app.get("/test", (req, res) => {
-  res.status(200).json("It's Working...");
-});
+// Middleware to verify API key
+function verifyAPIKey(req, res, next) {
+  const apiKey = req.query.apikey;
+  if (apiKey !== API_KEY) {
+      return res.status(401).json({ message: "Unauthorized. Invalid API key." });
+  }
+  next();
+}
 
-router.post("/api/v1/upload", controllers.datapost);
-router.get("/api/v1/data", controllers.getData);
-router.get("/filter", controllers.advancedatafilter);
-router.get("/api/v1/popular/:id", controllers.getonedata);
-router.delete("/api/v1/delete/:id", controllers.deletedata);
-router.put("/api/v1/popular/update/:dataid", controllers.updatedata);
+
+router.post("/api/v1/upload", verifyAPIKey, controllers.datapost);
+router.get("/api/v1/data", verifyAPIKey, controllers.getData);
+router.get("/filter", verifyAPIKey, controllers.advancedatafilter);
+router.get("/api/v1/popular/:id", verifyAPIKey, controllers.getonedata);
+router.delete("/api/v1/delete/:id", verifyAPIKey, controllers.deletedata);
+router.put("/api/v1/popular/update/:dataid", verifyAPIKey, controllers.updatedata);
 
 // Use the router for all routes defined above
 app.use("/", router);
@@ -31,3 +39,4 @@ app.use("/", router);
 app.listen(PORT, () => {
   console.log("Server Started");
 });
+
