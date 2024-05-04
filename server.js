9f166dcf7f5c127a805119a441335d84f4fdfdfe
gulshan-09 <152474356+gulshan-09@users.noreply.github.com>
2024-05-04 11:32:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,37 +14,31 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).json("You are quite clever but not more than me. You can't access my database.😉😎");
+  res.status(200).json("You are quite cleaver but not more than me. You can't access my database.😉😎");
 });
 
-// Middleware to verify request URL
-function verifyRequestURL(req, res, next) {
-    const host = req.headers.host;
-    if (!ALLOWED_HOSTS.includes(host)) {
-        return res.status(403).json({ message: "Forbidden. Host not allowed." });
-    }
-    next();
+// Middleware to verify API key and host
+function verifyRequest(req, res, next) {
+  const apiKey = req.query.apikey;
+
+  // Check if API key is valid
+  if (apiKey !== API_KEY) {
+      return res.status(401).json({ message: "Unauthorized. Invalid API key." });
+  }
+  
+  next();
 }
 
-// Middleware to verify API key
-function verifyAPIKey(req, res, next) {
-    const apiKey = req.query.apikey;
-    if (apiKey !== API_KEY) {
-        return res.status(401).json({ message: "Unauthorized. Invalid API key." });
-    }
-    next();
-}
-
-router.post("/api/v1/upload", verifyRequestURL, verifyAPIKey, controllers.datapost);
-router.get("/api/v1/data", verifyRequestURL, verifyAPIKey, controllers.getData);
-router.get("/filter", verifyRequestURL, verifyAPIKey, controllers.advancedatafilter);
-router.get("/api/v1/popular/:id", verifyRequestURL, verifyAPIKey, controllers.getonedata);
-router.delete("/api/v1/delete/:id", verifyRequestURL, verifyAPIKey, controllers.deletedata);
-router.put("/api/v1/popular/update/:dataid", verifyRequestURL, verifyAPIKey, controllers.updatedata);
+router.post("/api/v1/upload", verifyRequest, controllers.datapost);
+router.get("/api/v1/data", verifyRequest, controllers.getData);
+router.get("/filter", verifyRequest, controllers.advancedatafilter);
+router.get("/api/v1/popular/:id", verifyRequest, controllers.getonedata);
+router.delete("/api/v1/delete/:id", verifyRequest, controllers.deletedata);
+router.put("/api/v1/popular/update/:dataid", verifyRequest, controllers.updatedata);
 
 // Use the router for all routes defined above
 app.use("/", router);
 
 app.listen(PORT, () => {
-    console.log("Server Started");
+  console.log("Server Started");
 });
